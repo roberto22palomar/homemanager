f1cd31be12bd8c6c00aa27d30167b64c46450fc9
Rober <roberto22palomar@gmail.com>
2024-10-01 20:01:44 +0200

    Mejora al crear casa, incluye el creador a la lista de miembros desde el contexto de spring.

--- homeManager/src/main/java/com/example/homemanager/api/models/request/HouseRequest.java
+++ homeManager/src/main/java/com/example/homemanager/api/models/request/HouseRequest.java
@@ -14,7 +14,6 @@ import java.util.Set;
 public class HouseRequest {
 
     private String name;
-    private String creatorId;
     private Set<String> membersId;
     private Set<String> tasksId;
 

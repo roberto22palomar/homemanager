1f456758f436f0f62504faa53239ad262db87bcc
Rober <roberto22palomar@gmail.com>
2024-10-11 12:50:43 +0200

    Prueba cli github

--- homeManager/src/main/java/com/example/homemanager/HomeManagerApplication.java
+++ homeManager/src/main/java/com/example/homemanager/HomeManagerApplication.java
@@ -2,10 +2,12 @@ package com.example.homemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableMongoRepositories(basePackages = "com.example.homemanager.domain.repositories")
 public class HomeManagerApplication {
 
     public static void main(String[] args) {

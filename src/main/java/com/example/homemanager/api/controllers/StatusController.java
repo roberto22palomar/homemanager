3f09ae491e8e28c660d76abfac6624baa737d7d0
roberto22palomar <roberto22palomar@gmail.com>
2025-10-30 10:25:50 +0100

    Añadimos log para status del server

--- src/main/java/com/example/homemanager/api/controllers/StatusController.java
+++ src/main/java/com/example/homemanager/api/controllers/StatusController.java
@@ -3,6 +3,7 @@ package com.example.homemanager.api.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +16,14 @@ import java.util.Map;
 @RestController
 @RequestMapping(path = "status")
 @AllArgsConstructor
+@Slf4j
 @Tag(name = "Status requests", description = "Endpoints related to the server status")
 public class StatusController {
 
     @Operation(summary = "Estado del servidor.", description = "Petición para comprobar el estado del servidor.")
     @GetMapping("/health")
     public ResponseEntity<Map<String, Object>> health() {
+        log.info("Server health: OK");
         Map<String, Object> status = new HashMap<>();
         status.put("status", "UP");
         status.put("timestamp", Instant.now().toString());
